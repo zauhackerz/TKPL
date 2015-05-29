ead81271d10c9f9fc2b069bf0a17d88dcddfa29f
tantowi <tantowie@outlook.com>
2015-05-29 18:35:00 +0700

    file bespoke

---
+++ src/scripts/main.js
@@ -0,0 +1,36 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  cube = require('bespoke-theme-cube'),
+  //voltaire = require('bespoke-theme-voltaire'),
+  //terminal = require('bespoke-theme-terminal'),
+  //fancy = require('bespoke-theme-fancy');
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  progress = require('bespoke-progress'),
+  forms = require('bespoke-forms');
+
+// Bespoke.js
+bespoke.from('article', [
+  cube(),
+  //voltaire(),
+  //terminal(),
+  //fancy(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash(),
+  progress(),
+  forms()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
